0cae97dfe5f179d29170a74e2c2376d5f9399a91
Michael Hansen <michael.hansen.24@us.af.mil>
2019-06-12 13:47:39 -0400

    First successful test with Sonic Pi and LEGACY code

--- legacy.rb
+++ legacy.rb
@@ -1,55 +1,73 @@
-# Welcome to Sonic Pi v3.0.1
+# LEGACY 2019 code for Sonic Pi integration
+# Author: Michael Hansen
+# Date: 2019 June 12
+
 require 'thread'
 require 'json'
 
+# -----------------------------------------------------------------------------
+
+# Uses SDL to capture gamepad events.
+# Calls provided callback functions from a separate thread.
 module Gamepad
-  @button_callback = lambda {|b|}
-  @axis_callback = lambda {|a, v|}
-  @hat_callback = lambda {|v|}
+  @button_queue = Queue.new
 
+  # Numbers and names for buttons on Logitech F310
+  @button_map = {
+    0 => :a,
+    1 => :b,
+    2 => :x,
+    3 => :y,
+    4 => :lb,
+    5 => :rb,
+    6 => :back,
+    7 => :start,
+    8 => :logitech,
+    9 => :lstick,
+    10 => :rstick
+  }
+
+  # True if game loop is running
   @running = false
+  @thread = nil
 
-  def self.on_button(&block)
-    @button_callback = block
+  # Queue to add to when buttons are pressed
+  def self.button_queue
+    return @button_queue
   end
 
-  def self.on_axis(&block)
-    @axis_callback = block
+  # Map from index to button symbol name
+  def self.button_map
+    return @button_map
   end
 
-  def self.on_hat(&block)
-    @hat_callback = block
-  end
-
-  def self.button_callback
-    return @button_callback
-  end
-
-  def self.axis_callback
-    return @axis_callback
-  end
-
-  def self.hat_callback
-    return @hat_callback
-  end
-
+  # Game loop
   def self.run
-    if not @running
+    if not @running then
       @running = true
-      Thread.new do
+      @thread = Thread.new do
         require 'sdl'
-        SDL::init(SDL::INIT_EVERYTHING)
+        SDL::init(SDL::INIT_JOYSTICK)
         joy = SDL::Joystick.open(0)
 
-        loop do
-          event = SDL::Event2.poll
-          case event
-          when SDL::Event2::JoyButtonDown
-            Gamepad.button_callback.call(event.button)
-          when SDL::Event2::JoyAxis
-            Gamepad.axis_callback.call(event.axis, event.value)
-          when SDL::Event2::JoyHat
-            Gamepad.hat_callback.call(event.value)
+        # Index => true if button is down
+        button_down = {}
+
+        while @running do
+          # Doing this manually because events don't work in Sonic Pi
+          SDL::Joystick.update_all
+
+          # Check all buttons, track state
+          joy.num_buttons.times do |i|
+            already_down = button_down.fetch(i, false)
+            if joy.button(i) then
+              if not already_down
+                button_down[i] = true
+                @button_queue.push i
+              end
+            else
+              button_down[i] = false
+            end
           end
 
           sleep 0.05
@@ -57,28 +75,71 @@ module Gamepad
       end
     end
   end
+
+  # Stops game loop
+  def self.stop
+    @running = false
+    if @thread then
+      @thread.join
+      @thread = nil
+    end
+  end
+
 end
 
-def use_gamepad
+# -----------------------------------------------------------------------------
+
+# Start gamepad thread and handle button presses by name
+def use_gamepad(&block)
   Gamepad.run
+  in_thread(name: :gamepad) do
+    loop do
+      if not Gamepad.button_queue.empty?
+        button = Gamepad.button_queue.pop()
+        button_sym = Gamepad.button_map.fetch(button, nil)
+        block.call(button_sym)
+
+        File.open("/home/hansenm/button_callback.txt", "w") do |f|
+          f.write("#{button_sym}")
+        end
+      end
+      sleep 0.05
+    end
+  end
 end
 
-module Pixels
-  @pixel_queue = Queue.new
-  @pixel_count = 32
-  @pixels = [0] * 3 * @pixel_count
-  @running = false
+# -----------------------------------------------------------------------------
 
+# Communicates with WS2801 LED strip via SPI.
+# Does blending animations in separate thread.
+module Pixels
+  # Queue with function calls to be made in animation thread
+  @pixel_queue = Queue.new
+
+  # Number of LEDs
+  @pixel_count = 32
+
+  # BGR values for each LED
+  @pixels = [0] * 3 * @pixel_count
+
+  # True if animation thread is running
+  @running = false
+  @thread = nil
+
+  # SPI interface to WS2801
   @spi = nil
 
+  # Synchronization for @pixels
   @semaphore = Mutex.new
 
+  # Connects to WS2801
   def self.use_spi
     require 'spi'
     @spi = SPI.new(device: '/dev/spidev0.0')
     @spi.speed = 1000000
   end
 
+  # Shows colors values in @pixels
   def self.show
     @semaphore.synchronize do
       if @spi
@@ -92,6 +153,7 @@ module Pixels
     end
   end
 
+  # Sets RGB color of the ith LED
   def self.one!(i, r, g, b)
     i *= 3
     @semaphore.synchronize do
@@ -101,6 +163,7 @@ module Pixels
     end
   end
 
+  # Sets RGB color of all LEDs
   def self.all!(r, g, b)
     @semaphore.synchronize do
       @pixel_count.times do |i|
@@ -111,15 +174,18 @@ module Pixels
     end
   end
 
+  # Gets RGB color if the ith LED
   def self.one(i)
     i *= 3
     return [@pixels[i+2], @pixels[i+1], @pixels[i]]
   end
 
+  # Gets BGR colors of all LEDs
   def self.all
     return @pixels
   end
 
+  # Blends from current to new RGB color for all LEDs
   def self.blend_all(r, g, b, steps: 10, delay: 0.05, wait: true)
     thread = Thread.new do
       start = Array.new(@pixels)
@@ -133,6 +199,7 @@ module Pixels
         increment[i3+2] = (r - @pixels[i3+2]) / steps_f
       end
 
+      # Blend each animation step
       steps.times do |t|
         @semaphore.synchronize do
           @pixel_count.times do |i|
@@ -156,6 +223,7 @@ module Pixels
     end
   end
 
+  # Blends from current to new RGB colors for all LEDs
   def self.blend_rgb(rgb, steps: 10, delay: 0.05, wait: true)
     thread = Thread.new do
       start = Array.new(@pixels)
@@ -169,6 +237,7 @@ module Pixels
         increment[i3+2] = (rgb[i3] - @pixels[i3+2]) / steps_f
       end
 
+      # Blend each animation step
       steps.times do |t|
         @semaphore.synchronize do
           @pixel_count.times do |i|
@@ -183,6 +252,7 @@ module Pixels
         sleep delay
       end
 
+      # Ensure final values match input color list
       @semaphore.synchronize do
         @pixel_count.times do |i|
           i3 = i*3
@@ -200,6 +270,7 @@ module Pixels
     end
   end
 
+  # Blend from the current to new RGB color for the ith LED
   def self.blend_one(i, r, g, b, steps: 10, delay: 0.05, wait: true)
     thread = Thread.new do
       start = @pixels[i..(i+3)]
@@ -212,6 +283,7 @@ module Pixels
       increment[1] = (g - @pixels[i3+1]) / steps_f
       increment[2] = (r - @pixels[i3+2]) / steps_f
 
+      # Blend each animation step
       steps.times do |t|
         @semaphore.synchronize do
           @pixels[i3] = start[0] + (increment[0]*t).to_i
@@ -223,6 +295,7 @@ module Pixels
         sleep delay
       end
 
+      # Ensure the final value matches input RGB
       one!(i, r, g, b)
       show
     end
@@ -232,14 +305,22 @@ module Pixels
     end
   end
 
+  # ---------------------------------------------------------------------------
+
+  # Cycle all LED colors up (first LED color goes to last, second to first, etc.)
   def self.spin(steps: 10, delay: 0.05, wait: true)
     thread = Thread.new do
       steps = steps.to_i
+
+      # Do for specified number of steps
       steps.times do |t|
         @semaphore.synchronize do
+          # Save first LED colors
           b1 = @pixels[0]
           g1 = @pixels[1]
           r1 = @pixels[2]
+
+          # Shift all LEDs up
           (@pixel_count-1).times do |i|
             i3 = i*3
             j3 = (i+1)*3
@@ -249,6 +330,7 @@ module Pixels
             @pixels[i3+2] = @pixels[j3+2]
           end
 
+          # Set last LED to saved first LED values
           ilast = @pixels.length - 3
           @pixels[ilast] = b1
           @pixels[ilast+1] = g1
@@ -265,6 +347,7 @@ module Pixels
     end
   end
 
+  # Get rainbow color from specific position in color wheel
   def self.wheel(pos)
     if pos < 85
       return [pos * 3, 255 - pos * 3, 0]
@@ -277,6 +360,7 @@ module Pixels
     end
   end
 
+  # Get blended rainbow RGB values for LEDs
   def self.rainbow
     rgb = [0] * 3 * @pixel_count
     @pixel_count.times do |i|
@@ -290,18 +374,22 @@ module Pixels
     return rgb
   end
 
+  # Pause some number of seconds
   def self.doze(seconds, *args)
     sleep seconds
   end
 
+  # Run animation thread
   def self.run
     if not @running
       @running = true
-      Thread.new do
-        loop do
+      @thread = Thread.new do
+        while @running do
           # name, positional args, keyword args
           task = @pixel_queue.pop()
-          Pixels.send(task[0], *task[1], **task[2])
+          if task then
+            Pixels.send(task[0], *task[1], **task[2])
+          end
         end
       end
     end
@@ -311,17 +399,34 @@ module Pixels
     @pixel_queue.push(task)
   end
 
+  # Stops the main animation thread
+  def self.stop
+    @running = false
+    if @thread then
+      @pixel_queue.push(nil)
+      @thread.join
+      @thread = nil
+    end
+  end
+
 end
 
-def use_leds
+# -----------------------------------------------------------------------------
+
+# Enable LEDs and animation
+def use_leds(spi: true)
   Pixels.run
-  Pixels.use_spi
+  if spi then
+    Pixels.use_spi
+  end
 end
 
+# Pause animation thread
 def doze(seconds)
   Pixels.enqueue([:doze, [seconds], {}])
 end
 
+# Set RGB color(s) for all or ith LED
 def rgb (r, g, b, i: nil, time: 1)
   steps = time / 0.05
   if i then
@@ -376,12 +481,21 @@ def strobe (cycles, time: 0.075, r: 255, g: 255, b: 255)
   end
 end
 
+# Blend to rainbow colors
 def rainbow (time: 1)
   steps = time / 0.05
   Pixels.enqueue([:blend_rgb, [Pixels.rainbow], { steps: steps, wait: true }])
 end
 
+# Cycle LED colors up
 def spinner (time: 1)
   steps = time / 0.05
   Pixels.enqueue([:spin, [], { steps: steps, wait: true }])
+end
+
+# -----------------------------------------------------------------------------
+
+def stop_legacy
+  Gamepad.stop
+  Pixels.stop
 end
--- legacy_test.rb
+++ legacy_test.rb
@@ -1,5 +1,28 @@
+require 'thread'
 require './legacy.rb'
-Pixels.run
+
+# Test gamepad
+use_gamepad
+
+m = Mutex.new
+cv = ConditionVariable.new
+
+on_button :a do
+  m.synchronize do
+    cv.signal
+  end
+end
+
+# Wait until button press
+puts "Press the A button"
+m.synchronize do
+  cv.wait(m)
+end
+puts "OK"
+
+use_leds spi: false
+
+# Test LEDS
 black time: 0
 rainbow
 sleep 3
@@ -9,3 +32,7 @@ black time: 0
 red i: 1, time: 0
 spinner time: 5
 sleep 5
+black time: 0
+sleep 2
+
+puts "Done"
